b5ae39838c355d2c2d2e8633c287bce0d935a108
Alex Davyskiba <sanya.davyskiba@gmail.com>
2022-04-15 17:23:35 +0300

    Remove useless db access from settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -781,14 +781,3 @@ $settings['skip_permissions_hardening'] = TRUE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$databases['default']['default'] = array (
-  'database' => 'test',
-  'username' => 'user',
-  'password' => 'pass',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'z3E--oNvFSOTLZOS_VLA350WbRk0_8lsvFnIqFWjjMW4pa52UlemCsbSzx8VG_Ak-bauMANtNQ';
